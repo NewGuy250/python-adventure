45e41f1f096dd4617c7b6f39eb40b1fe6b95e754
NewGuy250 <ndaniel220@yahoo.com>
2024-11-04 23:33:31 -0500

    added get_name and greeting message

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+def get_name():
+    return input("Enter your name: ")
+
 def main():
-    pass
+    name = get_name()
+    print(f"Welcome, {name}, to this adventure!")
+
+if __name__ == "__main__":
+    main()
